d64da5454ee296a8011dbdbb70a92c2ce2556292
Pavel Kravchenko <pavlo.kravchenko@ab-soft.net>
2020-01-22 13:27:43 +0200

    JS homework

---
+++ tasks_from_learn_JS/Arrays/filterRange.js
@@ -0,0 +1,20 @@
+/**
+ * Created by pavlo.kravchenko on 1/22/2020.
+ */
+/*Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+
+ Функция должна возвращать новый массив и не изменять исходный.
+
+ Например:
+
+ let arr = [5, 3, 8, 1];
+
+ let filtered = filterRange(arr, 1, 4);
+
+ alert( filtered ); // 3,1 (совпадающие значения)
+
+ alert( arr ); // 5,3,8,1 (без изменений)*/
+
+function filterRange(arr, a, b) {
+    return arr.filter((item) => (item >= a && item <= b));
+}
