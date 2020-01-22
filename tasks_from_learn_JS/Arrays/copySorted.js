124e84bf0e1c3ab69b0e38302859f5e4ff8b9279
Pavel Kravchenko <pavlo.kravchenko@ab-soft.net>
2020-01-22 16:16:11 +0200

    JS homework

---
+++ tasks_from_learn_JS/Arrays/copySorted.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pavlo.kravchenko on 1/22/2020.
+ */
+/*Скопировать и отсортировать массив
+ важность: 5
+ У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+
+ Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+ let arr = ["HTML", "JavaScript", "CSS"];
+
+ let sorted = copySorted(arr);
+
+ alert( sorted ); // CSS, HTML, JavaScript
+ alert( arr ); // HTML, JavaScript, CSS (без изменений)*/
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
