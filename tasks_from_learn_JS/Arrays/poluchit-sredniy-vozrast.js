c9c17cae1c20c60d36017b798b9bed1abf7395f1
Pavel Kravchenko <pavlo.kravchenko@ab-soft.net>
2020-01-24 15:20:11 +0200

    JS homework

---
+++ tasks_from_learn_JS/Arrays/poluchit-sredniy-vozrast.js
@@ -0,0 +1,28 @@
+/**
+ * Created by pavlo.kravchenko on 1/24/2020.
+ */
+/*Получить средний возраст
+ важность: 4
+ Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+ Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+ Например:
+
+ let vasya = { name: "Вася", age: 25 };
+ let petya = { name: "Петя", age: 30 };
+ let masha = { name: "Маша", age: 29 };
+
+ let arr = [ vasya, petya, masha ];
+
+ alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28*/
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+function getAverageAge(users) {
+    return arr.reduce((tmp, item) => tmp + item.age, 0)/users.length;
+}
